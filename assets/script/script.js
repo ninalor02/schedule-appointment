83c32ef58989d9a8d4e1c081527d60e5f0bf8750
nina <128232339+ninalor02@users.noreply.github.com>
2023-06-25 19:19:42 -0500

    redo html/css/js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,27 +1,16 @@
 // ----> this is for the current live day and Date <---- //
 
 
-
-
 // date display
 // console.log('testing1'); //working//
 
 $(function () {
-  //testing purpose only console.log within the funciton
-  //  console.log('testing2'); //working//
 
-  // current day of the week //
   var dayWeek = dayjs();
-  //  console.log('dayWeek'); //working//
-  // current date //
+
   var today = dayjs();
-  //  console.log('today', today.format('MMM D, YYYY')); //working//
-
   $('#currentDay').text(today.format('MMM D, YYYY'));
-  //  console.log('#currentDay', $('#currentDay')); //working//
-
   $('#currentDay').text(dayWeek);
-  //  console.log('dayWeek1') //working//
 
 });
 // --> this is current live hours <---- //
@@ -33,11 +22,10 @@ $(function () {
 
 var textArea = $('#textArea');
 var saveBtn = $('.saveBtn');
-// console.log('#textArea', textArea);  //working//
-// console.log('.saveBtn', saveBtn);   //working//
+
 
 function scheduleFromStorage() {
-  // console.log('schedule send to storage');  //working//
+
   var hour9 = localStorage.getItem('hour-9');
   if (hour9) {
     $('#hour-9 .description').val(hour9);
@@ -79,16 +67,9 @@ function scheduleFromStorage() {
 saveBtn.on("click", function (event) {
   event.preventDefault();
 
-
-  //saveBtn.on('click', function (event) {  
-  //  console.log('appt saved', event.target);  // <--- jquery object, children, etc //
-  //  console.log('appt saved', $(this));
-
   var textArea = $(this).siblings('.description').val();
-  //  console.log(textDescription); //working//
-
   var timeBlock = $(this).parent().attr("id");
-  console.log(timeBlock);  //not working --> only saving under as timeblock//
+  console.log(timeBlock); 
 
   localStorage.setItem("timeBlock", textArea);
 
